8a56a32d80903975344fbc50c319ea30cea4888f
Felix Maldonado <felix_maldonado@hotmail.com>
2019-08-22 21:18:03 -0400

    Approved WireFrame

---
+++ Quiz.js
@@ -0,0 +1,62 @@
+const STORE = [
+    {
+        answers: ['September 9, 2014', 'December 20, 2015', 'April 30, 2015', 'September 19, 2013'],
+        correctIndx: 0,
+        extraLore: 'Destiny was released by developer, Bungie who is famous for originally developing the Halo Series.',
+        prompt: 'When was the original Destiny released?'
+    },
+    {
+        answers: ['Earth', 'Moon', 'Venus', 'Mars'],
+        correctIndx: 0,
+        extraLore: 'When first starting the game your ghost finds you laying as a corpse, in which it uses the light to reanimate you into a guardian.',
+        prompt: 'What planet are you on in the opening mission of Destiny 1?' 
+    },
+    {
+        answers: ['Zavala, Ikora, Cayde-6', 'Zavala, Cayde-6, Osiris', 'Ikora, Sloane, Caliban-8', 'Zavala, Shaxx, Saint-14'],
+        correctIndx: 0,
+        extraLore: 'Zavala is the Commander of the Vanguard and is responsible for coordinating the defense of the city, Zavala helps to mentor and outfit other Titans.',
+        prompt: 'Who are the members of the Vanguard at the launch of Destiny 2?' 
+    },
+    {
+        answers: ['Cabal', 'Fallen', 'Human', 'Taken'],
+        correctIndx: 0,
+        extraLore: 'He is also known as Ghost Primus',
+        prompt: 'What race is Dominus Ghual?' 
+    },
+    {
+        answers: ['Red Legion', 'Sand Eaters', 'Dust Giants', 'Blind Legion'],
+        correctIndx: 0,
+        extraLore: 'Ghual led a coup with the Red Legion against Empror Calus, which is what forced Calus to flee to the Laviathan',
+        prompt: 'Which legion does Dominus Ghual belong too?' 
+    },
+    {
+        answers: ['Crota', 'Ir Anuk', 'Xivu Arath', 'Auryx'],
+        correctIndx: 0,
+        extraLore: 'Crota wielded the Sword of Crota which he used to kill a large number of Guardians during the Vanguard offensive to retake the Moon.',
+        prompt: "What is Oryx's son named?" 
+    },
+    {
+        answers: ['Dreadnaught', 'Harbringer', 'Shrine', 'Falcon'],
+        correctIndx: 0,
+        extraLore: "Though the Dreadnaught is a Hive ship, its infact protected by Oryx's personal army of Taken.",
+        prompt: "What is the name of Oryx's ship?" 
+    },
+    {
+        answers: ['September 6, 2017', 'November 18, 2017', 'June 1, 2016', 'September 6, 2018'],
+        correctIndx: 0,
+        extraLore: 'Destiny 2 currently has an Xbox Meta Score of 86 out of 100.  At first it received a lot dissapointment from the community, though since the relase of the Foresaken expansion, the popularity has picked up.',
+        prompt: 'When did Destiny 2 release?' 
+    },
+    {
+        answers: ['The Leviathan', 'Revenge of Calus', 'Crotas End', 'Wrath of the machine'],
+        correctIndx: 0,
+        extraLore: 'Upon release of the Leviathan it was recommended you have a light level of 270-275.',
+        prompt: 'What was the first Raid in Destiny 2 called?' 
+    },
+    {
+        answers: ['Ace', 'Spades', 'Randy', 'Cayde-5'],
+        correctIndx: 0,
+        extraLore: 'Its been revealed that before becoming an Exo, Cayde-6 had a Wife and son whom he nicknamed Ace.  This was only known from found record fragments in Caydes exo memory',
+        prompt: 'What was the name of Cayde-6 son?' 
+    },
+]
