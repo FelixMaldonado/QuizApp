b2cc969b558a4256466846db0ffedb0496f60f59
Felix Maldonado <felix_maldonado@hotmail.com>
2019-08-23 15:54:13 -0400

    Update

---
+++ handleQuiz.js
@@ -0,0 +1,21 @@
+let question = 0;
+let score = 0;
+
+// This will render the page everytime a button is selected
+function renderPage(){
+    $('.greetingSubmitButton').click(function(event){
+        if (question <= STORE.length){
+            return ''
+        }
+    })
+}
+
+
+// This will keep track of the question you on
+function questionCounter(){}
+
+
+//This will keep track of the score
+function scoreCounter(){}
+
+//
