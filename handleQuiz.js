befd61fa1483bcda73cc9e3561b33b5140f9c4f8
Felix Maldonado <felix_maldonado@hotmail.com>
2019-08-31 08:14:16 -0400

    broken

--- handleQuiz.js
+++ handleQuiz.js
@@ -18,7 +18,10 @@ function renderPage(){
         $('.newPage').remove();
         $('body').attr('class', 'OpeningPage');
         $('.OpeningPage').append('<div class = newPage><header class="header">' +
-        '<h1 class="questionTitle"><img class="logo left" src="https://www.destructoid.com//ul/262753-destiny.jpg" alt="Destiny 1 Box Art">Destiny Quiz<img class="logo right" src="https://i.redd.it/qchrru6vt7dz.jpg" alt="Destiny 2 Logo"></h1>' +
+        '<h1 class="questionTitle"><img class="logo left" src="https://www.destructoid.com//ul/262753-destiny.jpg" alt="Destiny 1 Box Art">Destiny Quiz</h1>' +
+        '<div class="centeredLogo">'+
+                    '<img class="logo right" src="https://i.redd.it/qchrru6vt7dz.jpg" alt="Destiny 2 Logo">'+
+        '</div>'+
     '</header>' +
     '<main class="main">' +
         '<div class="quizCounter">' +
@@ -35,31 +38,39 @@ function renderPage(){
                                 '<label for="possibleAnswers4"><input id= "possibleAnswers4" type="radio" name="question1" value="3" required/>' + STORE[question].answers[3] + '</label><br>' +
                             
                             '</div>' +
-                            '<input class="questionsubmitButton" type="submit" value="Submit">' +
+                            '<div class="buttonHolder">'+
+                             '<input class="questionsubmitButton" type="submit" value="Submit">' +
+                            '</div>'+
                             '<div class="questions"></div>' +
                                     
                 '</fieldset>' +
         '</form></div>')
-        
     };
+
+    
 
 
 
 /*---------------------------------------------------------------------------------------------------------------------*/
 /*----------------------------------------------Renders Correct Page---------------------------------------------------*/
 function correctAnswer(){
-    console.log(STORE[question-1]);
+    // console.log(STORE[question-1]);
     $('.newPage').remove();
     $('.OpeningPage').append('<div class ="newPage"><header class="header">' +
-    '<h1 class="correctTitle"><img class="logo left" src="https://pwntastic-avatar-production.s3.amazonaws.com/uploads/user/avatar/264188/main_Cayde_6.png" alt="Cayde Thumbs Up">Correct!!!!!!<img class="logo right" src="https://i.imgur.com/qlTdyrR.jpg" alt="Happy Zavala"></h1>' +
+    '<h1 class="correctTitle"><img class="logo left" src="https://pwntastic-avatar-production.s3.amazonaws.com/uploads/user/avatar/264188/main_Cayde_6.png" alt="Cayde Thumbs Up">Correct!!!!!!</h1>' +
+    '<div class="centeredLogo">'+
+        '<img class="logo right" src="https://i.imgur.com/qlTdyrR.jpg" alt="Happy Zavala">'+
+    '</div>'+
 '</header>' +
 '<main class="correctMain">' +
     '<div class="correctCounter">' +
         '<p class="correctScore">' + rightScore + ' of 10 correct</p>' +
         '<p class="wrong">' + wrongScore + ' of 10 incorrect </p>' +
         '<p class="lore">' + STORE[question-1].extraLore + '</p>' +
-    '</div>' +      
-    '<input class="submitButton" type="submit" value="Next Question">' +        
+    '</div>' +
+    '<div class="buttonHolder">'+
+        '<input class="submitButton" type="submit" value="Next Question">' + 
+    '</div>'+               
 '</main></div>');
 $('body').attr('class', 'correct');
 }
@@ -68,9 +79,12 @@ $('body').attr('class', 'correct');
 function wrongAnswer(){
     $('.newPage').remove();
     let showAnswer = STORE[question-1].correctIndx;
-    console.log(showAnswer);
+    // console.log(showAnswer);
     $('.OpeningPage').append('<div class = "newPage"><header class="header">' +
-    '<h1 class="incorrectTitle"><img class="incorrectLogoLeft" src="https://img05.deviantart.net/201c/i/2015/166/8/5/oryx__the_taken_king_by_b1izzardhawkfa-d8xhffu.png" alt="Oryx">***Incorrect***<img class="incorrectLogoRight" src="https://cdn.vox-cdn.com/thumbor/xDzUsp6lU_D6tVJaZjpKxwjuiKg=/0x0:1920x1080/920x613/filters:focal(807x387:1113x693):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/64702054/calus.0.png" alt="Emperor Calus"></h1>' +
+    '<h1 class="incorrectTitle"><img class="logo left" src="https://img05.deviantart.net/201c/i/2015/166/8/5/oryx__the_taken_king_by_b1izzardhawkfa-d8xhffu.png" alt="Oryx">***Incorrect***</h1>' +
+    '<div class="centeredLogo">'+
+        '<img class="logo right" src="https://cdn.vox-cdn.com/thumbor/xDzUsp6lU_D6tVJaZjpKxwjuiKg=/0x0:1920x1080/920x613/filters:focal(807x387:1113x693):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/64702054/calus.0.png" alt="Emperor Calus">'+
+    '</div>'+
 '</header>' +
 '<main class="correctMain">' +
     '<div class="incorrectCounter">' +
@@ -78,7 +92,9 @@ function wrongAnswer(){
         '<p class="wrong">' + wrongScore + ' of 10 incorrect </p>' +
         '<p class="givenAnswer"> The correct answer was: ' + STORE[question-1].answers[showAnswer] + '</p>' +
     '</div>' +      
-'<input class="submitButton" type="submit" value="Next Question">' +        
+    '<div class="buttonHolder">'+
+        '<input class="submitButton" type="submit" value="Next Question">' + 
+    '</div>'+                 
 '</main></div>');
 $('body').attr('class', 'incorrect');
 }
